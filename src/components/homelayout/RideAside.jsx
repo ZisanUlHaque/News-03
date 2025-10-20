34f6087d211c142966f57c325c1ec1a61031bba5
Zisan <mdzisanulhaque989@gmail.com>
2025-10-20 18:08:39 +0600

    news add

--- src/components/homelayout/RideAside.jsx
+++ src/components/homelayout/RideAside.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
+import FindUs from '../FindUs';
+import Qzone from '../Qzone';
 
 const RideAside = () => {
     return (
-        <div>
+        <div className='space-y-8'>
             <SocialLogin></SocialLogin>
+            <FindUs></FindUs>
+            <Qzone></Qzone>
         </div>
     );
 };
--- src/components/homelayout/SocialLogin.jsx
+++ src/components/homelayout/SocialLogin.jsx
@@ -8,7 +8,7 @@ const SocialLogin = () => {
             <h2 className='font-bold mb-5'>Login with</h2>
             <div className='flex flex-col gap-3 '>
                 <button className='btn btn-secondary btn-outline w-full'><FcGoogle size={24}></FcGoogle> Login with Google</button>
-                <button className='btn btn-secondary btn-outline w-full'><FaGithub size={24}></FaGithub> Login with Github</button>
+                <button className='btn btn-primary btn-outline w-full'><FaGithub size={24}></FaGithub> Login with Github</button>
             </div>
         </div>
     );
